f08b7034c45de15cb3f4155df8261cbbc6768473
rasit <archesxd@gmail.com>
2022-12-03 23:14:13 +0300

    todoapp
    coded by Arches.

---
+++ ToDoArches/ToDoArches/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace ToDoArches
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.moveToDoing_BTN = new System.Windows.Forms.Button();
+            this.moveToDone_BTN = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.moveToDone2_BTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(85, 155);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(258, 314);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(464, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Doing";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(174, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Planned";
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 31;
+            this.listBox2.Location = new System.Drawing.Point(379, 155);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(258, 314);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(750, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Done";
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 31;
+            this.listBox3.Location = new System.Drawing.Point(660, 155);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(258, 314);
+            this.listBox3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 609);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Arches ❤️ C";
+            // 
+            // moveToDoing_BTN
+            // 
+            this.moveToDoing_BTN.Location = new System.Drawing.Point(101, 486);
+            this.moveToDoing_BTN.Name = "moveToDoing_BTN";
+            this.moveToDoing_BTN.Size = new System.Drawing.Size(258, 23);
+            this.moveToDoing_BTN.TabIndex = 7;
+            this.moveToDoing_BTN.Text = "Move To Doing";
+            this.moveToDoing_BTN.UseVisualStyleBackColor = true;
+            this.moveToDoing_BTN.Click += new System.EventHandler(this.moveToDoing_BTN_Click);
+            // 
+            // moveToDone_BTN
+            // 
+            this.moveToDone_BTN.Location = new System.Drawing.Point(101, 523);
+            this.moveToDone_BTN.Name = "moveToDone_BTN";
+            this.moveToDone_BTN.Size = new System.Drawing.Size(258, 23);
+            this.moveToDone_BTN.TabIndex = 8;
+            this.moveToDone_BTN.Text = "Move To Done";
+            this.moveToDone_BTN.UseVisualStyleBackColor = true;
+            this.moveToDone_BTN.Click += new System.EventHandler(this.moveToDone_BTN_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(379, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(379, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 53);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Add To Plans";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // moveToDone2_BTN
+            // 
+            this.moveToDone2_BTN.Location = new System.Drawing.Point(379, 486);
+            this.moveToDone2_BTN.Name = "moveToDone2_BTN";
+            this.moveToDone2_BTN.Size = new System.Drawing.Size(258, 23);
+            this.moveToDone2_BTN.TabIndex = 11;
+            this.moveToDone2_BTN.Text = "Move To Done";
+            this.moveToDone2_BTN.UseVisualStyleBackColor = true;
+            this.moveToDone2_BTN.Click += new System.EventHandler(this.moveToDone2_BTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1033, 634);
+            this.Controls.Add(this.moveToDone2_BTN);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.moveToDone_BTN);
+            this.Controls.Add(this.moveToDoing_BTN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Arches To Do App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button moveToDoing_BTN;
+        private System.Windows.Forms.Button moveToDone_BTN;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button moveToDone2_BTN;
+    }
+}
+
---
+++ ToDoArches/ToDoArches/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoArches
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try 
+            { 
+                string eklenecekItem = textBox1.Text;
+                listBox1.Items.Add(eklenecekItem);
+                textBox1.Text = null;
+            } 
+            catch  (Exception sa )
+            {
+                MessageBox.Show(sa.Message);
+            }
+        }
+
+        private void moveToDoing_BTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if (listBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("please select a work");
+                }
+                else { 
+                    listBox2.Items.Add(listBox1.SelectedItem);
+
+                    listBox1.Items.Remove(listBox1.SelectedItem);
+                }
+
+            }
+            catch (Exception sa)
+            {
+                MessageBox.Show(sa.Message);
+            }
+        }
+
+        private void moveToDone_BTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (listBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("please select a work");
+                }
+                else
+                {
+                    listBox3.Items.Add(listBox1.SelectedItem);
+
+                    listBox1.Items.Remove(listBox1.SelectedItem);
+                }
+
+            }
+            catch (Exception sa)
+            {
+                MessageBox.Show(sa.Message);
+            }
+        }
+
+        private void moveToDone2_BTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (listBox2.SelectedItem == null)
+                {
+                    MessageBox.Show("please select a work");
+                }
+                else
+                {
+                    listBox3.Items.Add(listBox2.SelectedItem);
+
+                    listBox2.Items.Remove(listBox2.SelectedItem);
+                }
+
+            }
+            catch (Exception sa)
+            {
+                MessageBox.Show(sa.Message);
+            }
+        }
+    }
+}
